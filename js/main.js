2bbc706ddb4669918356c6f2a97c97964e286d8b
FikratRH <fikratrh@code.edu.az>
2023-03-27 00:41:54 +0400

    completed main part

--- js/main.js
+++ js/main.js
@@ -286,12 +286,17 @@ let currentCategory;
 const filterBtns = document.querySelectorAll(
   ".main_products_trending_title_categories_category"
 );
-const slider_wrapper = document.querySelector(".splide__list");
+const slider_wrapper = document.querySelector(
+  ".main_products_trending_boxes_wrapper_list"
+);
 const cart_btn_header = document.querySelector(".header_cart_number");
+const sliderItems = document.querySelector(".main_items_boxes_wrapper_list");
 
 document.addEventListener("DOMContentLoaded", function () {
   slider_wrapper.innerHTML = "";
+  sliderItems.innerHTML = "";
   dataArr.forEach((item) => {
+    console.log(dataArr.indexOf(item));
     slider_wrapper.innerHTML += `
     <li class="splide__slide" data-category="${item.category.toLowerCase()}" data-id="${dataArr.indexOf(
       item
@@ -341,15 +346,100 @@ document.addEventListener("DOMContentLoaded", function () {
     </div>
   </li>
     `;
+    sliderItems.innerHTML += `
+    <li class="splide__slide" data-category="${item.category.toLowerCase()}" data-id="${dataArr.indexOf(
+      item
+    )}">
+    <div class="main_products_trending_boxes_box">
+      <div class="main_products_trending_boxes_box_image">
+        <img
+          src="${item.img}"
+          alt=""
+          class="main_products_trending_boxes_box_image_img"
+        />
+        <div
+          class="main_products_trending_boxes_box_image_btns"
+        >
+          <a
+            href="#" onclick="return false" 
+            class="main_products_trending_boxes_box_image_btns_btn cart_btn"
+          >
+            <i class="fa-solid fa-cart-shopping"></i></a
+          ><a
+            href="#" onclick="return false" 
+            class="main_products_trending_boxes_box_image_btns_btn"
+            ><i class="fa-solid fa-heart"></i></a
+          ><a
+            href="#" onclick="return false" 
+            class="main_products_trending_boxes_box_image_btns_btn"
+            ><i class="fa-solid fa-magnifying-glass-plus"></i
+          ></a>
+        </div>
+      </div>
+      <div class="main_products_trending_boxes_box_desc">
+        <a
+          href="#"
+          class="main_products_trending_boxes_box_desc_title"
+          >${item.title} for ${item.category}</a
+        >
+        <span
+          class="main_products_trending_boxes_box_desc_price"
+        >
+          ${item.price}
+          <span
+            class="main_products_trending_boxes_box_desc_price_old"
+            >${item.oldPrice}</span
+          >
+        </span>
+      </div>
+    </div>
+  </li>
+    `;
   });
+
   currentCategory = "men";
-  var splide = new Splide(".splide", {
+
+  new Splide("#trending", {
     type: "loop",
     perPage: 4,
     perMove: 1,
-  });
+    pagination: false,
+    width: "100%",
+    breakpoints: {
+      992: {
+        perPage: 3,
+      },
+      778: {
+        perPage: 2,
+        arrows: false,
+      },
+    },
+  }).mount();
 
-  splide.mount();
+  new Splide("#items", {
+    type: "loop",
+    perPage: 4,
+    perMove: 1,
+    pagination: false,
+    width: "100%",
+    breakpoints: {
+      992: {
+        perPage: 3,
+      },
+      778: {
+        perPage: 2,
+        arrows: false,
+      },
+    },
+  }).mount();
+
+  new Splide("#testimonials", {
+    type: "loop",
+    perPage: 1,
+    perMove: 1,
+    pagination: false,
+    width: "100%",
+  }).mount();
 });
 
 filterBtns.forEach((btn) => {
@@ -420,16 +510,41 @@ function filterProducts(category) {
   </li>
     `;
   });
-  var splide = new Splide(".splide", {
+
+  new Splide("#trending", {
     type: "loop",
     perPage: 4,
     perMove: 1,
-  });
-
-  splide.mount();
+    breakpoints: {
+      992: {
+        perPage: 3,
+      },
+      778: {
+        perPage: 2,
+        arrows: false,
+      },
+    },
+  }).mount();
 }
 
 slider_wrapper.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("cart_btn") ||
+    e.target.parentElement.classList.contains("cart_btn")
+  ) {
+    console.log("cart click");
+    const id = e.target.closest("li").dataset.id;
+    const product = dataArr[id];
+    if (cart.includes(product)) {
+      alert("This product is already in your cart");
+    } else {
+      cart.push(product);
+      console.log(cart);
+      cart_btn_header.innerHTML = cart.length;
+    }
+  }
+});
+sliderItems.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("cart_btn") ||
     e.target.parentElement.classList.contains("cart_btn")
